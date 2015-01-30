1c3f945c41c603514046eb219ec4e68a6079dec8
Scott Kruger <scott@chojin.org>
2015-01-29 21:18:23 -0600

    Bump version

--- figgis.py
+++ figgis.py
@@ -1,4 +1,4 @@
-__version__ = '1.2.0'
+__version__ = '1.3.0'
 __all__ = ['Field', 'ListField', 'Config', 'ValidationError', 'PropertyError']
 
 
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 
     setup(
         name='figgis',
-        version='1.2.0',
+        version='1.3.0',
 
         description="Checked YAML configuration",
         long_description=CHANGELOG,
